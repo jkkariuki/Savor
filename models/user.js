42482401c9f065afa8279464b7c9faeaf0c8dac9
madsabate <meynard.anthony.sabate@gmail.com>
2018-01-31 22:42:21 -0500

    Authentication v0.2

--- models/user.js
+++ models/user.js
@@ -1,46 +1,16 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
+const passportLocalMongoose = require ('passport-local-mongoose');
+
+const User = new Schema ({
+    email: String,
+    password: {type: String, select: false}
+
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
 
 // User schema for database
-
-const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        index: {
-            unique: true
-        }
-    },
-    password: String
-});
-
-// Compare password with value in database. model method
-
-UserSchema.methods.comparePassword = function comparePassword(password, callback) {
-    bcrypt.compare(password, this.password, callback);
-};
-
-UserSchema.pre('save', function saveHook(next) {
-    const user = this;
-
-    if (!user.isModified('password')) return next();
-
-    return bcrypt.genSalt((saltError, salt) => {
-        if (saltError) {
-            return next(saltError);
-        }
-
-        return bcrypt.hash(user.password, salt, (hashError, hash) => {
-            if (hashError) {
-                return next(hashError);
-            }
-            // replace a password string with has h value
-            user.password = hash;
-
-            return next();
-        });
-    });
-});
-
-
-
-module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,37 +1,45 @@
 const express = require("express");
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const savorController = require("./controllers/savorController");
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+//Controllers
+const savorController = require("./controllers/savorController");
+const users = require('./client/src/utils/usersAPI');
 
 // Authentication Packages
 const passport = require('passport');
-
-// connect to the database and load models
-require('./models/user');
-
+const LocalStrategy = require('passport-local').Strategy;
 
 // Configure body parser for axios requests
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(bodyParser.json());
+// Cookie Parser middleware initialization
+app.use(cookieParser());
+// Initialize express session
+app.use(require('express-session')({
+  secret: 'lskjklfsj',
+  resave: false,
+  saveUninitialized: false
+}));
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
 // Serve up static assets
 app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(savorController);
-// Passport middleware
-app.use(passport.initialize());
+app.use(users);
 
-// Passport strategies
-const localSignupStrategy = require('./controllers/userController');
-const localLoginStrategy = require('./controllers/loginController');
-passport.use('local-signup', localSignupStrategy);
-passport.use('local-login', localLoginStrategy);
-
-// pass authentication checker middleware
-// const authCheckMiddleware = require('./ser') 
+const User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Set up promises with mongoose
 mongoose.Promise = Promise;
